63aa38b4529f97a4f60b093a4188e3420d3f2935
Derek Marion <derek.j.marion@gmail.com>
2023-10-04 12:18:25 -0700

    First commit for working fibonacci function that satisfies all test cases

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,10 @@
 def fibonacci(n):
-  pass
-
-
+    fib = [0, 1] # seed first two numbers in fibonacci sequence
+    if n <= 1:  # handle cases where n is 0 or 1
+        return fib[n]
+    i = 2
+    while i <= n:  # build fib sequence up to place n
+        current_value = fib[i - 2] + fib[i - 1]
+        fib.append(current_value)
+        i += 1
+    return fib[-1]  # return last value of fib sequence
